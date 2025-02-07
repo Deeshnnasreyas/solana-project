463e25241a2e511acd5d4a4feff249593f3ee6cb
deeshnna <deeshnnasreyas@gmail.com>
2025-02-07 15:34:06 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/Router";
+import Router from "./routes/router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={Router} />
   </StrictMode>
 );
